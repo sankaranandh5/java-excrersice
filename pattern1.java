1763d42c0c9a31873dcbcbf7b02019c89e5925c5
sankaranandh <sankarsanandh@gmail.com>
2017-07-25 22:33:24 +0530

    seventh

---
+++ pattern1.java
@@ -0,0 +1,27 @@
+package com.qspiders.qwerty;
+
+public class pattern1 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++){
+       for(int j=1;j<=5;j++){
+   		System.out.print(i);
+       }
+       System.out.println();
+	}
+}
+}
+/* 
+	11111
+	22222
+i	33333
+	44444
+	55555
+ */
+
+/*
+	12345
+	12345
+j	12345
+	12345
+	12345
+*/
---
+++ pattern2.java
@@ -0,0 +1,27 @@
+package com.qspiders.qwerty;
+
+public class pattern2 {
+public static void main(String[] args) {
+	for(int i=5;i>=1;i--){
+	       for(int j=5;j>=1;j--){
+	   		System.out.print(i);
+	       }
+	       System.out.println();
+		}
+}
+}
+/*
+	55555
+	44444
+i	33333
+	22222
+	11111
+ */
+
+/*
+	54321
+	54321
+j	54321
+	54321
+	54321
+*/
