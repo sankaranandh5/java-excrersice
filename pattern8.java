1e8633a52d25b24a94b94979bebda830530bdc94
sankaranandh <sankarsanandh@gmail.com>
2017-07-27 11:14:16 +0530

    nineth

---
+++ pattern8.java
@@ -0,0 +1,19 @@
+package com.qspiders.qwerty;
+
+public class pattern8 {
+public static void main(String[] args) {
+	for(char i='A';i<='E';i++){
+		for(char j='A';j<='E';j++){
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+}
+}
+/*
+	ABCDE
+	ABCDE
+	ABCDE
+	ABCDE
+	ABCDE
+*/
---
+++ pattern9.java
@@ -0,0 +1,26 @@
+package com.qspiders.qwerty;
+
+public class pattern9 {
+public static void main(String[] args) {
+	for(int i=5;i>=1;i--){
+		for(int j=i;j>=1;j--){
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+}
+/*
+	55555
+	4444
+i	333
+	22
+	1
+*/
+/*
+	54321
+	4321
+j	321
+	21
+	1
+*/
