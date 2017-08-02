b16c2457b6c68acaa564135b9ed844c017a0bad3
sankaranandh <sankarsanandh@gmail.com>
2017-08-02 22:57:43 +0530

    twelfth

---
+++ Pattern23.java
@@ -0,0 +1,23 @@
+package com.qspiders.qwerty;
+
+public class Pattern23 {
+public static void main(String[] args) {
+	int n=5;
+	for(int i=n;i>=1;i--){
+		for(int j=n;j>i;j--){
+			System.out.print(" ");
+		}
+		for(int k=i-1;k>=-(i-1);k--){
+			System.out.print(1+Math.abs(k));
+		}
+		System.out.println();
+	}
+}
+}
+/*
+		543212345
+		 4321234
+		  32123
+		   212
+		    1
+*/
